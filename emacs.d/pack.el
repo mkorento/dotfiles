ef4894778969e44bc708a58a0b78124f10b816a4
Mika Korento <korento.mika@gmail.com>
2018-05-19 23:10:23 +0300

    Add emacs packages

--- emacs.d/pack.el
+++ emacs.d/pack.el
@@ -22,7 +22,9 @@
   :config (global-undo-tree-mode))
 
 (use-package window-number
-  :ensure window-number)
+  :ensure window-number
+  :config
+  (window-number-mode 1))
 
 (use-package avy
   :ensure avy
@@ -31,3 +33,22 @@
   (setq
    avy-keys
    '(?h ?u ?t ?e ?n ?o ?s ?a ?d ?i ?b ?k ?g ?p ?c ?. ?r ?, ?f ?x ?w ?v ?; ?l ?')))
+
+(use-package whitespace
+  :ensure whitespace
+  :config
+  (setq whitespace-line-column 80)
+  (setq whitespace-style '(face tab-mark lines-tail))
+  (setq whitespace-display-mappings
+            '((tab-mark   ?\t [?\xBB])))
+  (global-whitespace-mode +1))
+
+(use-package zop-to-char
+  :ensure t
+  :bind (("M-z" . zop-up-to-char)
+         ("M-Z" . zop-to-char)))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode +1))
